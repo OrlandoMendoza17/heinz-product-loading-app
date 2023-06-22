ee5de9116f33ae28edab5cba0a713110d5648bba
Orlando Mendoza <ommv.17@gmail.com>
2023-06-22 11:06:34 -0400

    Configuración de headers de seguridad

--- next.config.js
+++ next.config.js
@@ -1,4 +1,48 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+  poweredByHeader: false,
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'strict-transform-security',
+            value: 'max-age=31536000; includeSubdomains',
+          },
+          // {
+          //   key: 'content-security-policy',
+          //   value: '',
+          // },
+          {
+            key: 'referrer-policy',
+            value: 'no-referrer',
+          },
+          {
+            key: 'x-frame-options',
+            value: 'DENY',
+          },
+          // {
+          //   key: 'permission-policy',
+          //   value: '',
+          // },
+          {
+            key: 'x-content-type-options',
+            value: 'nosniff',
+          },
+          {
+            key: 'x-permitted-cross-domain-policies',
+            value: 'none',
+          },
+          {
+            key: 'clear-site-data',
+            value: '*',
+          },
+        ],
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
